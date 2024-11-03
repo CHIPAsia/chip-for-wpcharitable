117fef5bf8298590e1b9d4c624740077777baa53
qir4ni <qiraniawis@gmail.com>
2024-11-04 01:42:34 +0800

    Add function process_donation()

--- includes/gateway/class-charitable-gateway-chip.php
+++ includes/gateway/class-charitable-gateway-chip.php
@@ -170,11 +170,70 @@ if (!class_exists('Charitable_Gateway_Chip'))
          * @static
          * @since   1.0.0
          */
-        public static function redirect_to_processing($return, $donation_id)
+        public static function redirect_to_processing_legacy($donation_id)
+        {
+            wp_safe_redirect(
+                charitable_get_permalink('donation_processing_page',
+                    array(
+                        'donation_id' => $donation_id
+                    )));
+            
+            exit();
+        }
+        // public static function redirect_to_processing($return, $donation_id)
+        // {
+
+       
+        // }
+
+        /**
+         * Process the donation with CHIP
+         */
+        public static function process_donation($content, Charitable_Donation $donation)
+        {
+            error_log('Inside process_donation function');
+            // new Charitable_Gateway_Chip
+            $gateway = new Charitable_Gateway_Chip();
+
+            // Check for currency MYR
+            if (charitable_get_option('currency') != "MYR") {
+                $method_data['errors'][] = "CHIP only accept payments in Ringgit Malaysia (RM)";
+            } else {
+                $method_data = $gateway->createRequest($donation, $gateway);
+            }
+
+            // If method_data['action] set
+            if (isset($method_data['action'])) {
+                echo $content;
+                echo ("<form method='get'
+	               action='" . $method_data['action'] . "'	id='chip-form'></form>
+                   <script type='text/javascript'>        
+                       function charitable_submit_chip_form() {
+                            var form = document.getElementById('chip-form');
+                            form.submit();
+	                   }
+	
+	                   window.onload = charitable_submit_chip_form();
+	               </script>");
+            } else if (isset($method_data['errors'])) {
+                echo ("<div class='charitable-notice charitable-form-errors'>Error:");
+                foreach ($method_data['errors'] as $e) {
+                    echo ("<div>" . $e . "</div>");
+                }
+                echo ("<a href=" . $gateway->getCancelUrl($donation) . ">Go Back</a></div>");
+            }
+
+            $content = ob_get_clean();
+            return $content;
+
+        }
+
+        /**
+         * Create request for donation and gateway API
+         */
+        private function createRequest($donation, $gateway)
         {
 
-            $gateway = new Charitable_Gateway_Chip();
-            $donation = charitable_get_donation($donation_id);
             /*
              * Support for Advance CHIP for WP Charitable Plugin
              */
@@ -190,133 +249,144 @@ if (!class_exists('Charitable_Gateway_Chip'))
                 }
             }
 
-            // Checking if currency is MYR
-            if ("MYR" != charitable_get_option('currency')) 
-			{
-                $error_notice = "CHIP only accepts payments in Ringgit Malaysia(RM).";
-				
-				echo ("<div class='charitable-notice charitable-form-errors'>Error:");
-               	echo ("<div>".$error_notice."</div>");
-				echo ("<a href=".$gateway->get_cancel_url($donation).">Go Back</a></div>");
-            } 
-            else 
-            {
-                $donor = $donation->get_donor();
+            $donor = $donation->get_donor();
 
-                $first_name = $donor->get_donor_meta('first_name');
-                $last_name = $donor->get_donor_meta('last_name');
-                $name = $first_name . ' ' . $last_name;
-                $email = $donor->get_donor_meta('email');
-                $mobile = $donor->get_donor_meta('phone');
-                $amount = $donation->get_total_donation_amount(true);
+            $first_name = $donor->get_donor_meta('first_name');
+            $last_name = $donor->get_donor_meta('last_name');
+            $name = $first_name . ' ' . $last_name;
 
-                $product_info 	= html_entity_decode($donation->get_campaigns_donated_to(), ENT_COMPAT, 'UTF-8' );
-                $donation_key	= $donation->get_donation_key();
-                $payment_gateway =  $donation->get_gateway();
+            $email = $donor->get_donor_meta('email');
+            $mobile = $donor->get_donor_meta('phone') ?? '';
+            $amount = $donation->get_total_donation_amount(true);
 
-                error_log('Campaign Name: ' . $campaign_name);
+            $product_info 	= html_entity_decode($donation->get_campaigns_donated_to(), ENT_COMPAT, 'UTF-8' );
+            $donation_key	= $donation->get_donation_key();
+            $payment_gateway =  $donation->get_gateway();
 
-                $keys = $gateway->get_keys();
+            $keys = $gateway->get_keys();
 
-                /*
-                * The filter use it to get secret key from campaign meta key
-                * Example: empty($campaign->get('chip_secret_key')) ? $secret_key : $campaign->get('chip_secret_key');
-                */
-
-                $brand_id = apply_filters('chip_for_wp_charitable_brand_id', $keys['brand_id'], $post, $campaign);
-                $secret_key = apply_filters('chip_for_wp_charitable_secret_key', $keys['secret_key'], $post, $campaign);
-                $ipn_url = Charitable_Gateway_Chip_IPN_Listener::get_listener_url($donation);
+            $brand_id = apply_filters('chip_for_wp_charitable_brand_id', $keys['brand_id'], $post, $campaign);
+            $secret_key = apply_filters('chip_for_wp_charitable_secret_key', $keys['secret_key'], $post, $campaign);
+            $ipn_url = Charitable_Gateway_Chip_IPN_Listener::get_listener_url($donation);
 
 
-                error_log('Brand ID: ' . $brand_id);
-                // Response URL
-                // $cancel_url = charitable_get_permalink('donation_cancel_page', array('donation_id' => $donation->ID));
-                // $response_url	= get_option( 'siteurl' ).'/donation-receipt/'.$donation->ID.'';
-                $success_url 	= charitable_get_permalink('donation_receipt_page',array('donation_id' => $donation->ID));
-                // $campaign_donation_url = charitable_get_permalink( 'campaign_donation_page', array( 'campaign_id' => $campaign->ID));
-                // $campaign_url = charitable_get_permalink( 'campaign_donation', array( 'campaign_id' => $campaign->ID));
-                $cancel_url = charitable_get_permalink('donation_cancellation', array('donation_id' => $donation->ID));
+            error_log('Brand ID: ' . $brand_id);
+            // Response URL
+            // $cancel_url = charitable_get_permalink('donation_cancel_page', array('donation_id' => $donation->ID));
+            // $response_url	= get_option( 'siteurl' ).'/donation-receipt/'.$donation->ID.'';
+            $success_url = charitable_get_permalink('donation_receipt_page',array('donation_id' => $donation->ID));
+            $success_url = add_query_arg('donation_key', $donation_key, $success_url);
+            // $campaign_donation_url = charitable_get_permalink( 'campaign_donation_page', array( 'campaign_id' => $campaign->ID));
+            // $campaign_url = charitable_get_permalink( 'campaign_donation', array( 'campaign_id' => $campaign->ID));
+            $cancel_url = charitable_get_permalink('donation_cancellation', array('donation_id' => $donation->ID));
 
-                // Params
-                $purchase_params = array(
-                    'client' => array(
-                        'email' => $email,
-                        'full_name' => $name
-                        // 'phone'
-                    ),
-                    'success_redirect' => $success_url, // the donation receipt url page
-                    // 'failure_redirect' => '',
-                    'cancel_redirect' => $cancel_url, // donation cancel page
-                    // 'success_callback' => $ipn_url, // ipn callback 
-                    'success_callback' => 'https://webhook.site/4fb2208a-e322-457b-8335-e074832760de', // testing purpose
-                    'creator_agent'    => 'WP Charitable',
-                    'reference'        => $donation->ID, 
-                    // 'client_id'        => $client['id'],
-                    'platform'         => 'api', // 'charitable'
-                    // 'send_receipt'     => $params['purchaseSendReceipt'] == 'on',
-                    // 'due'              => time() + (abs( (int)$params['dueStrictTiming'] ) * 60),
-                    'brand_id'         => $brand_id,
-                    'purchase'         => array(
-                    //   'timezone'   => $params['purchaseTimeZone'],
-                    'currency'   => 'MYR',
-                    //   'due_strict' => $params['dueStrict'] == 'on',
-                    'products'   => array([
-                        'name'     => substr($campaign_name, 0, 256),
-                        'price'    => round($amount * 100),
-                    ]),
-                    ),
-                );
-                
+            // Params
+            $purchase_params = array(
+                'client' => array(
+                    'email' => $email,
+                    'full_name' => $name
+                    // 'phone'
+                ),
+                'success_redirect' => $success_url, // the donation receipt url page
+                // 'failure_redirect' => '',
+                'cancel_redirect' => $cancel_url, // donation cancel page
+                // 'success_callback' => $ipn_url, // ipn callback 
+                'success_callback' => 'https://webhook.site/4fb2208a-e322-457b-8335-e074832760de', // testing purpose
+                'creator_agent'    => 'WP Charitable',
+                'reference'        => $donation->ID, 
+                // 'client_id'        => $client['id'],
+                'platform'         => 'api', // 'charitable'
+                // 'send_receipt'     => $params['purchaseSendReceipt'] == 'on',
+                // 'due'              => time() + (abs( (int)$params['dueStrictTiming'] ) * 60),
+                'brand_id'         => $brand_id,
+                'purchase'         => array(
+                //   'timezone'   => $params['purchaseTimeZone'],
+                'currency'   => 'MYR',
+                //   'due_strict' => $params['dueStrict'] == 'on',
+                'products'   => array([
+                    'name'     => substr($campaign_name, 0, 256),
+                    'price'    => round($amount * 100),
+                ]),
+                ),
+            );
+            
 
-                // Check first if brand ID and secret key configured
-                $credentials = array(
-                    $secret_key,
-                    $brand_id
-                );
-                $chip = new Chip($credentials);
+            // Check first if brand ID and secret key configured
+            $credentials = array(
+                $secret_key,
+                $brand_id
+            );
+            $chip = new Chip($credentials);
 
-                error_log('Private Key: ' . $chip->display());
+            // error_log('Private Key: ' . $chip->display());
 
-                // error_log(var_dump($chip->create_payment($purchase_params)));
+            // error_log(var_dump($chip->create_payment($purchase_params)));
 
-                $response = $chip->create_payment($purchase_params);
+            $response = $chip->create_payment($purchase_params);
 
-                // error_log($response);
-                // die(var_dump($chip));
 
-                // die();
-                // $bill_url = $chip->getURL();
-                // $bill_id = $chip->getID();
+            // Check if success or fail
+            // $bill_url = $response['checkout_url'];
+            // $bill_id = $response['id'];
 
-                // Check if success or fail
-                $bill_url = $response['checkout_url'];
-                $bill_id = $response['id'];
+            // error_log('Bill ID: ' . $bill_id);
 
-                error_log('Bill ID: ' . $bill_id);
+            // Set the gateway trxn id
+            // $donation->set_gateway_transaction_id($bill_id);
 
-                // Set the gateway trxn id
-                $donation->set_gateway_transaction_id($bill_id);
-
-                update_option('chip_charitable_bill_id_' . $bill_id, $donation->ID, false);
-
+            // Check if have response from CHIP
+            if (isset($response['id'])) {
+                $method_data['action'] = $response['checkout_url'];
+                $donation->set_gateway_transaction_id($response['id']);
+                self::update_donation_log($donation, "Checkout Link: " . print_r($response['checkout_url'], true));
+                // $donation->update_donation_log("Checkout Link: " . print_r($response['checkout_url']));
+                // update_option('chip_charitable_bill_id_' . $bill_id, $donation->ID, false);
             }
 
-            // wp_redirect( $url );
-            // exit;
+            return $method_data;
+        }
 
-            // wp_safe_redirect(
-            //     charitable_get_permalink('donation_processing_page',
-            //         array(
-            //             'donation_id' => $donation_id
-            //         )));
-            error_log('Redirecting to gate.chip-in.asia');
-            return wp_redirect($response['checkout_url']);
-            exit();
+        /**
+         * Get cancel URL 
+         */
+        private function getCancelUrl($donation)
+        {
+            $cancel_url = charitable_get_permalink('donation_cancel_page',
+                array(
+                    'donation_id' => $donation->ID
+                ));
+            
+            if (! $cancel_url) {
+                $cancel_url = esc_url(
+                    add_query_arg(
+                        array(
+                            'donation_id' => $donation->ID,
+                            'cancel' => true
+                        ), wp_get_referer()));
+            }
+            return $cancel_url;
+        }
 
-            // return array(
-            //     'redirect' => $bill_url,
-            //     'safe' => true, // false
-            // );
+        /**
+         * Update Charitable Donation Log
+         */
+        // public static function update_donation_log($donation, $message)
+        // {
+        //     // Check Charitable version
+
+        //     return $donation->update_donation_log($message);
+        // }
+
+        /**
+         * Update the donation's log. 		 
+         */
+        public static function update_donation_log($donation, $message)
+        {
+            if (version_compare(charitable()->get_version(), '1.4.0', '<')) {
+                return Charitable_Donation::update_donation_log($donation->ID, $message);
+            }
+
+            return $donation->update_donation_log($message);
         }
 
         /**
@@ -329,26 +399,9 @@ if (!class_exists('Charitable_Gateway_Chip'))
          */
         public static function process_response(Charitable_Donation $donation)
         {
+            error_log('Running process_response() function');
             return;
         }
-
-        /**
-         * Update the donation's log. 
-         *
-         * @return  void
-         * @access  public
-         * @static 
-         * @since   1.1.0		 
-         */
-        public static function update_donation_log($donation, $message)
-        {
-            if (version_compare(charitable()->get_version(), '1.4.0', '<')) {
-                return Charitable_Donation::update_donation_log($donation->ID, $message);
-            }
-
-            return $donation->update_donation_log($message);
-        }
-
 
         /**
          * Get the Gateway Notice
