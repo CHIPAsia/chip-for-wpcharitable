78fc0346c2b57d3430ec82fc34312b118c859c50
qir4ni <qiraniawis@gmail.com>
2024-11-12 16:27:26 +0800

    Use Wordpress plugin dependencies

--- charitable-chip.php
+++ charitable-chip.php
@@ -2,6 +2,7 @@
 
 /**
  * Plugin Name: CHIP for WP Charitable
+ * Requires Plugins: charitable
  * Plugin URI: https://github.com/CHIPAsia/chip-for-wpcharitable
  * Description: CHIP - Digital Finance Platform
  * Version: 1.0.0
@@ -9,6 +10,7 @@
  * Copyright: © 2024 CHIP
  * License: GNU General Public License v3.0
  * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ * 
  */
 
 // Exit if accessed directly.
@@ -22,25 +24,7 @@ if ( ! defined( 'ABSPATH' ) ) {
  */
 function charitable_chip_load() {
 	require_once( 'includes/class-charitable-chip.php' );
-
-	$has_dependencies = true;
-
-	/* Check for Charitable */
-	if ( ! class_exists( 'Charitable' ) ) {
-
-		if ( ! class_exists( 'Charitable_Extension_Activation' ) ) {
-
-			require_once 'includes/class-charitable-extension-activation.php';
-		}
-
-		$activation = new Charitable_Extension_Activation( plugin_dir_path( __FILE__ ), basename( __FILE__ ) );
-		$activation = $activation->run();
-
-		$has_dependencies = false;
-	} else {
-
-		new Charitable_Chip( __FILE__ );
-	}
+	new Charitable_Chip( __FILE__ );
 }
 
 add_action( 'plugins_loaded', 'charitable_chip_load', 1 );
